4dbefb3003300a7828c18d1ec9a514c98d4a42ec
princess sutha <jayasuthaitdepartment2020@gmail.com>
2022-05-19 20:14:30 +0530

    this is for my own test

---
+++ file2.py
@@ -0,0 +1,11 @@
+# basic if else statement
+from numpy import average
+
+
+a = int(input("enter your first mark :"))
+b = int(input("enter your second mark :"))
+c = int(input("enter your third mark :"))
+total = a+b+c
+average = total/3
+print("total mark is :", total)
+print("average is :", average)
